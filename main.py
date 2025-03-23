0a88c8144d34731812707f21f860481b109f1941
0.0.0 <arhipsurovtsev34@gmail.com>
2025-03-23 21:41:44 +0900

    0.0.0 First Code

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('Just beginning')
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((600, 480))
+pygame.display.set_caption('Stickman Dungeon Adventure')
+running = True
+while running:
+
+
+    pygame.display.update()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+
